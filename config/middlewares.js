eb20968343ce52d834615ffe3569fe4f3b8a5a72
Hamilton Silva <hamiltonsilva551@gmail.com>
2023-04-27 15:49:42 +0100

    cool

--- config/middlewares.js
+++ config/middlewares.js
@@ -6,32 +6,7 @@ module.exports = [
   'strapi::logger',
   'strapi::query',
   'strapi::body',
-  {
-    name: "strapi::security",
-    config: {
-      contentSecurityPolicy: {
-        useDefaults: true,
-        directives: {
-          "connect-src": ["'self'", "https:"],
-          "img-src": [
-            "'self'",
-            "data:",
-            "blob:",
-            "storage.googleapis.com",
-            "dl.airtable.com",
-          ],
-          "media-src": [
-            "'self'",
-            "data:",
-            "blob:",
-            "storage.googleapis.com",
-            "dl.airtable.com",
-          ],
-          upgradeInsecureRequests: null,
-        },
-      },
-    },
-  },
+  "strapi::security",
   'strapi::favicon',
   'strapi::public',
 ];
